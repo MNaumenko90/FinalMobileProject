1c8e30e19ea47c614bdba156e0ab36a439a4a9b3
MNaumenko <maria.igorevna9@gmail.com>
2024-03-01 18:34:08 +0200

    test

---
+++ src/main/java/config/builders/LoginCreds.java
@@ -0,0 +1,18 @@
+package config.buiders;
+
+public class LoginCreds {
+    private String username;
+    private String password;
+
+    public LoginCreds(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
